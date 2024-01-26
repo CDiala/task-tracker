86148f2159d98e798716d8eee951413ea2b00d8f
CDiala <dialachibuzo@yahoo.com>
2024-01-26 19:57:29 +0100

    chore: Install Angular Material dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // standalone: true,
+  // imports: [MatButtonModule],
 })
 export class AppComponent {
   title = 'task-tracker';
