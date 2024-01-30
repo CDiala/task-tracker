b44399ee49e0b7e9c8d313bb958e181572be98fd
CDiala <dialachibuzo@yahoo.com>
2024-01-30 10:45:34 +0100

    feat: Enable data-exchange between form & parent component

--- src/app/components/kanban-form/kanban-form.component.ts
+++ src/app/components/kanban-form/kanban-form.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AngularMaterialModule } from 'src/app/shared/angular-material/angular-material.module';
 import { InputGroupComponent } from '../input-group/input-group.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InputDateComponent } from '../input-date/input-date.component';
+import { ITask } from 'src/app/interfaces/i-user';
 
 @Component({
   selector: 'app-kanban-form',
@@ -11,33 +12,37 @@ import { InputDateComponent } from '../input-date/input-date.component';
   styleUrl: './kanban-form.component.css',
   imports: [AngularMaterialModule, InputGroupComponent, InputDateComponent],
 })
-export class KanbanFormComponent {
+export class KanbanFormComponent implements OnInit {
   taskForm!: FormGroup;
+  @Input({ alias: 'taskInfo', required: false }) taskInfo: ITask | undefined;
+  @Output() closeEvent: EventEmitter<Event> = new EventEmitter();
+  @Output() submitEvent: EventEmitter<ITask> = new EventEmitter();
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log('form data', this.taskInfo);
     this.taskForm = this.fb.group({
-      title: ['', Validators.required],
-      description: ['', [Validators.required, Validators.email]],
-      due_date: ['', [Validators.required]],
+      title: [this.taskInfo ? this.taskInfo.title : '', Validators.required],
+      description: [
+        this.taskInfo ? this.taskInfo.description : '',
+        [Validators.required, Validators.email],
+      ],
+      due_date: [
+        this.taskInfo ? this.taskInfo.due_date : '',
+        [Validators.required],
+      ],
+      status: [this.taskInfo ? this.taskInfo.status : 'open'],
     });
   }
 
-  taskTitleInfo = {
-    isPassword: false,
-    placeholder: 'Task details...',
-    error: '',
-    label: 'Task Title',
-  };
-
-  handleFormSubmit() {
-    console.log('button clicked', this.taskForm.value);
+  handleFormSubmit(event: Event) {
+    this.submitEvent.emit(this.taskForm.value as ITask);
   }
 
-  handleFormClose() {
-    console.log('button clicked');
+  handleFormClose(event: Event) {
+    this.closeEvent.emit(event);
   }
 
-  getDateValue(event: Event) {
-    console.log(event);
-  }
+  getDateValue(event: Event) {}
 }
